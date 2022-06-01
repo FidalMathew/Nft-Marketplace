3be5eedf62578025a426290d445933b708176b97
Fidal <fidal15perfect@gmail.com>
2022-06-01 12:59:25 +0530

    hardhat env set

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
